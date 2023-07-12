6f2d3946ad33fec8aedc104508d00889ce87bcea
pablomoty <pablomoty@gmail.com>
2023-07-11 22:31:42 -0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+  formulario: FormGroup;
+
+  constructor() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      correo: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl(''),
+      ciudad: new FormControl('', [Validators.required])
+    });
+  }
+
+  submitForm() {
+    if (this.formulario.valid) {
+      
+    } else {
+      
+    }
+  }
+}
